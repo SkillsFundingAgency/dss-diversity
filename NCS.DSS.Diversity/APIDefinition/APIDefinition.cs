d0ecfbd8f4d05ee8ebb58b759036442c4e0fa87f
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-06-21 12:24:06 +0100

    Add Example to API definition

--- NCS.DSS.Diversity/APIDefinition/APIDefinition.cs
+++ NCS.DSS.Diversity/APIDefinition/APIDefinition.cs
@@ -123,7 +123,7 @@ namespace NCS.DSS.Diversity.APIDefinition
                 foreach (string verb in verbs)
                 {
                     dynamic operation = new ExpandoObject();
-                    operation.operationId = ToTitleCase(functionAttr.Name) + ToTitleCase(verb);
+                    operation.operationId = ToTitleCase(functionAttr.Name);
                     operation.produces = new[] { "application/json" };
                     operation.consumes = new[] { "application/json" };
                     operation.parameters = GenerateFunctionParametersSignature(methodInfo, route, doc);
@@ -370,8 +370,11 @@ namespace NCS.DSS.Diversity.APIDefinition
                 dynamic propDef = new ExpandoObject();
                 propDef.description = GetPropertyDescription(property);
 
-                var stringAttribute = (StringLengthAttribute)property.GetCustomAttributes(typeof(StringLengthAttribute), false).FirstOrDefault();
+                var exampleAttribute = (Example)property.GetCustomAttributes(typeof(Example), false).FirstOrDefault();
+                if (exampleAttribute != null)
+                    propDef.example = exampleAttribute.Description;
 
+                var stringAttribute = (StringLengthAttribute)property.GetCustomAttributes(typeof(StringLengthAttribute), false).FirstOrDefault();
                 if (stringAttribute != null)
                 {
                     propDef.maxLength = stringAttribute.MaximumLength;
@@ -379,11 +382,8 @@ namespace NCS.DSS.Diversity.APIDefinition
                 }
 
                 var regexAttribute = (RegularExpressionAttribute)property.GetCustomAttributes(typeof(RegularExpressionAttribute), false).FirstOrDefault();
-
                 if (regexAttribute != null)
-                {
                     propDef.pattern = regexAttribute.Pattern;
-                }
 
                 SetParameterType(property.PropertyType, propDef, definitions);
                 AddToExpando(objDef.properties, property.Name, propDef);
@@ -473,7 +473,7 @@ namespace NCS.DSS.Diversity.APIDefinition
                     if (string.IsNullOrEmpty(description))
                         description = item.ToString();
 
-                    enumValues.Add(description);
+                    enumValues.Add(Convert.ToInt32(item) + " - " + description);
                 }
 
                 opParam.@enum = enumValues.ToArray();
--- NCS.DSS.Diversity/Models/Diversity.cs
+++ NCS.DSS.Diversity/Models/Diversity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using NCS.DSS.Diversity.Annotations;
 using NCS.DSS.Diversity.ReferenceData;
 
 namespace NCS.DSS.Diversity.Models
@@ -7,48 +8,60 @@ namespace NCS.DSS.Diversity.Models
     public class Diversity
     {
         [Display(Description = "Unique identifier for a diversity record")]
+        [Example(Description = "b8592ff8-af97-49ad-9fb2-e5c3c717fd85")]
         public Guid DiversityId { get; set; }
 
         [Required]
         [Display(Description = "Unique identifier of a customer.")]
+        [Example(Description = "2730af9c-fc34-4c2b-a905-c4b584b0f379")]
         public Guid CustomerId { get; set; }
 
         [Required]
         [Display(Description = "Indicator to say consent to collect special category LLDD Health data was given by the customer.")]
+        [Example(Description = "false")]
         public bool ConsentToCollectLLDDHealth { get; set; }
 
         [Required]
         [Display(Description = "LLDD Health Problem Declaration reference data.")]
+        [Example(Description = "1")]
         public LLDDHealthProblemDeclaration LLDDHealthProblemDeclaration { get; set; }
 
         [Required]
         [Display(Description = "Primary LLDD Heath Problem reference data.")]
+        [Example(Description = "4")]
         public PrimaryLLDDHeathProblem PrimaryLLDDHeathProblem { get; set; }
 
         [Required]
         [Display(Description = "Secondary LLDD Heath Problem reference data.")]
+        [Example(Description = "5")]
         public SecondaryLLDDHeathProblem SecondaryLLDDHeathProblem { get; set; }
 
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time the LLDD Heath consent was collected from the customer.")]
+        [Example(Description = "2018-06-21T17:45:00")]
         public DateTime DateAndTimeLLDDHealthConsentCollected { get; set; }
 
         [Display(Description = "Indicator to say consent to collect special category ethnicity data was given by the customer.")]
+        [Example(Description = "true")]
         public bool ConsentToCollectEthnicity { get; set; }
 
         [Required]
         [Display(Description = "Ethnicity reference data.")]
+        [Example(Description = "31")]
         public Ethnicity Ethnicity { get; set; }
 
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time ethnicity data was collected from the customer")]
+        [Example(Description = "2018-06-21T14:45:00")]
         public DateTime DateAndTimeEthnicityCollected { get; set; }
 
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time of the last modification to the record.")]
+        [Example(Description = "2018-06-21T12:17:00")]
         public DateTime LastModifiedDate { get; set; }
 
         [Display(Description = "Identifier of the touchpoint who made the last change to the record")]
+        [Example(Description = "d1307d77-af23-4cb4-b600-a60e04f8c3df")]
         public Guid LastModifiedBy { get; set; }
     } 
 }
