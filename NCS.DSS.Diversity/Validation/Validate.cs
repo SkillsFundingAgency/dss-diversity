24f7a91c07675e85e11a857265acd367461ab3a6
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-02 15:57:06 +0100

    Initial checkin for Post using Cosmos

---
+++ NCS.DSS.Diversity/Validation/Validate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NCS.DSS.Diversity.Validation
+{
+    public class Validate
+    {
+        public List<ValidationResult> ValidateResource<T>(T resource)
+        {
+            var context = new ValidationContext(resource, null, null);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(resource, context, results, true);
+
+            return isValid ? null : results;
+        }
+    }
+}
