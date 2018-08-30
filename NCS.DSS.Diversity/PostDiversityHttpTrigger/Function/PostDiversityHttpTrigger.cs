3f14281e06125e8e07faaa4d936b8fe6fe6506af
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2018-08-30 15:44:32 +0100

    add read only for post

--- NCS.DSS.Diversity/PostDiversityHttpTrigger/Function/PostDiversityHttpTrigger.cs
+++ NCS.DSS.Diversity/PostDiversityHttpTrigger/Function/PostDiversityHttpTrigger.cs
@@ -74,6 +74,11 @@ namespace NCS.DSS.Diversity.PostDiversityHttpTrigger.Function
             if (!doesCustomerExist)
                 return HttpResponseMessageHelper.NoContent(customerGuid);
 
+            var isCustomerReadOnly = await resourceHelper.IsCustomerReadOnly(customerGuid);
+
+            if (isCustomerReadOnly)
+                return HttpResponseMessageHelper.Forbidden(customerGuid);
+
             var doesDiversityDetailsExist = postDiversityService.DoesDiversityDetailsExistForCustomer(customerGuid);
 
             if (doesDiversityDetailsExist)
