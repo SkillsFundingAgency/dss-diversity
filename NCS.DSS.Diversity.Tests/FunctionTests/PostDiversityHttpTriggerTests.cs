9ab117b86036480fa0b2a1721e852feadc271ecb
Joe Hudson-Small <Joehs@me.com>
2023-01-27 12:09:17 +0000

    Update PostDiversityHttpTriggerTests.cs
    uncomments code

--- NCS.DSS.Diversity.Tests/FunctionTests/PostDiversityHttpTriggerTests.cs
+++ NCS.DSS.Diversity.Tests/FunctionTests/PostDiversityHttpTriggerTests.cs
@@ -78,7 +78,7 @@ namespace NCS.DSS.Diversity.Tests.FunctionTests
             _httpRequestHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns("0000000001");
             _httpRequestHelper.Setup(x => x.GetDssApimUrl(_request)).Returns("http://localhost:7071/");
             _resourceHelper.Setup(x => x.DoesCustomerExist(It.IsAny<Guid>())).Returns(Task.FromResult(true));
-            //_guidHelper.Setup(x => x.ValidateGuid(ValidCustomerId)).Returns(CustomerGuid);
+            _guidHelper.Setup(x => x.ValidateGuid(ValidCustomerId)).Returns(CustomerGuid);
 
             _httpRequestHelper.Setup(x => x.GetResourceFromRequest<Models.Diversity>(_request)).Returns(Task.FromResult(_diversity));
             _resourceHelper.Setup(x => x.DoesCustomerExist(It.IsAny<Guid>())).Returns(Task.FromResult(true));
@@ -101,7 +101,7 @@ namespace NCS.DSS.Diversity.Tests.FunctionTests
         [Test]
         public async Task PostDiversityHttpTrigger_ReturnsStatusCodeBadRequest_WhenCustomerIdIsInvalid()
         {
-            //_guidHelper.Setup(x => x.ValidateGuid(ValidCustomerId)).Returns(Guid.Empty);
+            _guidHelper.Setup(x => x.ValidateGuid(ValidCustomerId)).Returns(Guid.Empty);
 
             var result = await RunFunction(InValidCustomerId);
 
